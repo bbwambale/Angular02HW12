7566cce7834ef9861e57875a8be5402d042abc89
bbwambale <basiyonah033@gmail.com>
2018-07-12 16:09:53 -0500

    editted Directives
    editted Directives

--- src/app/myvisibility.directive.ts
+++ src/app/myvisibility.directive.ts
@@ -5,28 +5,11 @@ import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/
 })
 export class MyvisibilityDirective {
    @Input('bolInput')bolInput;
-  //private isHovering: boolean = false;
 
   constructor(private el:ElementRef, private re:Renderer2) { 
-    //el.nativeElement.style.backgroundcolor ='gray';
-    //re.setStyle(el.nativeElement, 'backgroundColor', 'gray');
-  }
-/* 
-  @HostListener('mouseover')
-  onMouseOver(){
-    // window.alert('Hovered over');
-    //let punchLineEL = this.el.nativeElement.querySelector('.orderedList');
-    this.re.setStyle(this.el.nativeElement, 'display', 'none');
-    //this.isHovering = true;
+
   }
 
-  @HostListener('mouseout')
-  onMouseOut(){
-    // window.alert('Hovered over');
-    //let punchLineEL = this.el.nativeElement.querySelector('.orderedList');
-    this.re.setStyle(this.el.nativeElement, 'display', 'block');
-    //this.isHovering = false;
-  } */
      
   ngOnInit(){
     if(this.bolInput == false){
